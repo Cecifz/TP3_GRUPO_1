385efb9137238dc592be7b0980d71f534e5bb16c
julietachapur <juli.chapur@hotmail.com>
2022-09-17 23:44:03 -0300

    modificaciones en principal

--- src/ejercicio1/Persona.java
+++ src/ejercicio1/Persona.java
@@ -4,8 +4,32 @@ import javax.swing.*;
 
 public class Persona {
     private int dni;
-
+    private String nombre;
+	private static String apellido;
+    
     public Persona(){  }
+    
+    public Persona(int dni, String nombre, String apellido){ 
+    	this.dni = dni;
+    	this.nombre = nombre;
+    	this.apellido = apellido;
+    }
+    
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
     public int getDni() {
         return dni;
@@ -25,4 +49,12 @@ public class Persona {
         }
     }
 
+    public static boolean findMax(Persona p2) {
+    	//mayor será true
+    	String persona2 = p2.apellido;
+    	//persona1 es menor
+		if(apellido.compareTo(persona2)<0) return true;
+		//persona2 es mayor o igual
+		if(apellido.compareTo(persona2)>=0) return false;
+	}
 }
--- src/ejercicio1/Principal.java
+++ src/ejercicio1/Principal.java
@@ -1,6 +1,20 @@
 package ejercicio1;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.swing.*;
+
+import ejemplo1_Equals.Persona;
+import ejemplo4_HashSet.Libro;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.io.BufferedReader;
+
 
 public class Principal {
 
@@ -9,12 +23,60 @@ public class Principal {
         //Instancia Persona, pide DNI y lo valida.
         // Si est√° ok lo pasa a int, lo setea y muestra. Sino muestra cartel de error.
         Persona persona = new Persona();
-        String dni = JOptionPane.showInputDialog("Ingrese DNI: ");
+        /*String dni = JOptionPane.showInputDialog("Ingrese DNI: ");
 
         if ( Persona.validarDNI(dni) ) {
             persona.setDni(Integer.parseInt(dni));
             System.out.println(persona.getDni());
         }
+        */
+        
+        FileReader entrada;
+        ArrayList<Persona> listaPersonas = new ArrayList<Persona>();
+		try {
+		   entrada = new FileReader("./files/Personas.txt");
+		   BufferedReader miBuffer = new BufferedReader(entrada);
+			
+		   String linea = "";
+		   while(true) {
+			    linea = miBuffer.readLine();
+			    System.out.println(linea);
+			    if(linea == null) {
+			    	break;
+			    }
+			    String[] user = linea.split("-");
+				String dni = user[2];
+				System.out.println(dni);
+				//verifica que el dni sea v·lido
+		        try {
+		        	if(Persona.validarDNI(dni))
+		        	{
+		        		listaPersonas.add(new Persona(Integer.parseInt(user[2]),user[0],user[1]));
+		        	}
+		        }
+				catch (Exception e) {
+				}
+			}
+		   	System.out.println("llego hasta el final");
+			miBuffer.close();
+			entrada.close();
 
-    }
+		} catch (IOException e) {
+			System.out.println("No se encontro el archivo");
+		}
+		
+	  /*Iterator<Persona> it = listaPersonas.iterator();
+	  Iterator<Persona> it2 = listaPersonas.iterator();
+	  while(it.hasNext())
+	  {
+		  Persona p1 = (Persona)it;
+		  while(it2.hasNext()) {
+			  boolean tipo = Persona.findMax(it2.next());
+			  System.out.println(tipo);
+			  System.out.println(p1);
+		  }
+	  }		  
+	  }*/
+
 }
+    }
